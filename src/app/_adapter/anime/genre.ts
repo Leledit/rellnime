90e1c2a9ef4d973db55b235353cc086c29327e5f
Leledit <profile.caixeta99@gmail.com>
2024-02-13 19:14:55 -0300

    Correction in gender exclusion adapters (anime/movies)

--- src/app/_adapter/anime/genre.ts
+++ src/app/_adapter/anime/genre.ts
@@ -2,13 +2,21 @@
 
 import { delet, post } from "../http.service";
 
-export async function adapterAnimeAddGenre(idAnime: string, nameGenre: string,tolken:string) {
+export async function adapterAnimeAddGenre(
+  idAnime: string,
+  nameGenre: string,
+  tolken: string
+) {
   const url = process.env.URL_API_BASE + "/animes/genres/add";
 
-  const result = await post(url, {
-    id: idAnime,
-    nameGenre: nameGenre,
-  },tolken);
+  const result = await post(
+    url,
+    {
+      id: idAnime,
+      nameGenre: nameGenre,
+    },
+    tolken
+  );
 
   if (result.status !== 200) {
     return result.status;
@@ -19,14 +27,16 @@ export async function adapterAnimeAddGenre(idAnime: string, nameGenre: string,to
   return resutlData;
 }
 
+export async function adapterAnimeDeleteGenre(
+  idAnime: string,
+  nameGenre: string,
+  tolken: string
+) {
+  const url =
+    process.env.URL_API_BASE +
+    `/animes/genres/delete?id=${idAnime}&nameGenre=${nameGenre}`;
 
-export async function adapterAnimeDeleteGenre(idAnime: string, nameGenre: string,tolken:string) {
-  const url = process.env.URL_API_BASE + "/animes/genres/delete";
-
-  const result = await delet(url, {
-    id: idAnime,
-    nameGenre: nameGenre,
-  },tolken);
+  const result = await delet(url, tolken);
 
   if (result.status !== 200) {
     return result.status;
--- src/app/_adapter/films/genre.ts
+++ src/app/_adapter/films/genre.ts
@@ -2,13 +2,21 @@
 
 import { delet, post } from "../http.service";
 
-export async function adapterFilmeAddGenre(idFilme: string, nameGenre: string,tolken:string) {
+export async function adapterFilmeAddGenre(
+  idFilme: string,
+  nameGenre: string,
+  tolken: string
+) {
   const url = process.env.URL_API_BASE + "/filmes/genres/add";
 
-  const result = await post(url, {
-    id: idFilme,
-    nameGenre: nameGenre,
-  },tolken);
+  const result = await post(
+    url,
+    {
+      id: idFilme,
+      nameGenre: nameGenre,
+    },
+    tolken
+  );
 
   if (result.status !== 200) {
     return result.status;
@@ -19,13 +27,16 @@ export async function adapterFilmeAddGenre(idFilme: string, nameGenre: string,to
   return resutlData;
 }
 
-export async function adapterFilmeDeleteGenre(idAnime: string, nameGenre: string,tolken:string) {
-  const url = process.env.URL_API_BASE + "/filmes/genres/delete";
+export async function adapterFilmeDeleteGenre(
+  idAnime: string,
+  nameGenre: string,
+  tolken: string
+) {
+  const url =
+    process.env.URL_API_BASE +
+    `/filmes/genres/delete?id=${idAnime}&nameGenre=${nameGenre}`;
 
-  const result = await delet(url, {
-    id: idAnime,
-    nameGenre: nameGenre,
-  },tolken);
+  const result = await delet(url, tolken);
 
   if (result.status !== 200) {
     return result.status;
